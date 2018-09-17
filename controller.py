17621a182cffb91539b1073fbce82fa10039bf1b
Anton Weiss <anton@otomato.link>
2018-09-18 00:25:12 +0300

    use objects instead of yaml. take care of destinationrule, start querying prometheus

--- controller.py
+++ controller.py
@@ -2,14 +2,39 @@ import json
 import yaml
 from kubernetes import client, config, watch
 import os, time
+import requests
 import monkeypatches.monkeypatch
 
 DOMAIN = "otomato.link"
-
+PROM_URL = "http://prometheus.159.8.233.5.nip.io" # http://prometheus.default.svc.cluster.local"
 def check_canary_health():
     print("Checking canary health ")
+    query_string = 'sum_over_time(flask_request_count{endpoint="/version",http_status="500",instance="aleph.games.svc.cluster.local:8000",job="aleph",method="GET"}[5m])'
+    response = requests.get(PROM_URL+"/api/v1/query?query="+query_string).content
+    metric = json.loads(response)
+    print metric["data"]["result"][0]["value"][1]
+
     time.sleep(5)
     return True
+
+
+def update_destination_rule(crds, namespace, service):
+    destination_rule = crds.get_namespaced_custom_object("networking.istio.io", 
+                                                         "v1alpha3", 
+                                                         namespace,
+                                                         "destinationrules",
+                                                         service)
+    #update destination rule
+    canary_index = next((index for (index, d) in enumerate(destination_rule["spec"]["subsets"]) 
+                                                                if d["name"] == "canary"), None)
+    destination_rule["spec"]["subsets"][canary_index]["labels"]["version"] = obj["spec"]["canary_version"]
+    crds.patch_namespaced_custom_object("networking.istio.io", 
+                                        "v1alpha3", 
+                                        namespace, 
+                                        "destinationrules", 
+                                        name, 
+                                        destination_rule)
+     
 
 def update_virtualservice(crds, obj):
     metadata = obj.get("metadata")
@@ -24,31 +49,25 @@ def update_virtualservice(crds, obj):
     canary_healthy = True
     prodWeight = 99
     canaryWeight = 1
+    
+    update_destination_rule(crds, namespace, service)
+
+    vs = crds.get_namespaced_custom_object("networking.istio.io", "v1alpha3", namespace, "virtualservices", service)
+    canary_index = next((index for (index, d) in enumerate(vs["spec"]["http"][0]["route"]) 
+                                                                if d["destination"]["subset"] == "canary"), None)
+    prod_index = next((index for (index, d) in enumerate(vs["spec"]["http"][0]["route"]) 
+                                                                if d["destination"]["subset"] == "production"), None)
+    vs["metadata"].pop("annotations")
+    vs["metadata"].pop("resourceVersion")    
+    vs["metadata"].pop("uid")   
+    vs["metadata"].pop("selfLink") 
+    
+    
     while canary_healthy and canaryWeight <=100:
         print("canary %s prod %s " %(canaryWeight, prodWeight))
-        document = """
-apiVersion: networking.istio.io/v1alpha3
-kind: VirtualService
-metadata:
-  name: {name}
-spec:
-  hosts:
-  - {name}
-  http:
-  - route:
-    - destination:
-        host: {name}
-        port:
-          number: 80
-        subset: v02
-      weight: {prod}
-    - destination:
-        host: aleph
-        port:
-          number: 80
-        subset: v03
-      weight: {canary}""".format(name=name, prod=prodWeight, canary=canaryWeight)
-        crds.patch_namespaced_custom_object("networking.istio.io", "v1alpha3", namespace, "virtualservices", name, yaml.load(document))
+        vs["spec"]["http"][0]["route"][canary_index]["weight"] = canaryWeight
+        vs["spec"]["http"][0]["route"][prod_index]["weight"] = prodWeight
+        crds.patch_namespaced_custom_object("networking.istio.io", "v1alpha3", namespace, "virtualservices", name, vs)
         canary_healthy = check_canary_health()
         prodWeight-=1
         canaryWeight+=1
