d3fbad6b669d4f29b42912d9ab56942b6bf97991
Anton Weiss <anton@otomato.link>
2018-09-18 16:39:46 +0300

    almost ready

--- controller.py
+++ controller.py
@@ -6,17 +6,24 @@ import requests
 import monkeypatches.monkeypatch
 
 DOMAIN = "otomato.link"
-PROM_URL = "http://prometheus.159.8.233.5.nip.io" # http://prometheus.default.svc.cluster.local"
-def check_canary_health():
-    print("Checking canary health ")
-    query_string = 'sum_over_time(flask_request_count{endpoint="/version",http_status="500",instance="aleph.games.svc.cluster.local:8000",job="aleph",method="GET"}[5m])'
-    response = requests.get(PROM_URL+"/api/v1/query?query="+query_string).content
-    metric = json.loads(response)
-    print metric["data"]["result"][0]["value"][1]
+PROM_URL = os.getenv("PROMETHEUS_URL", "http://prometheus.159.8.233.5.nip.io")  # http://prometheus.default.svc.cluster.local"
 
-    time.sleep(5)
+def check_canary_health(metric, healthy, deviation):
+    print("Checking canary health ")
+    current = retrieve_metric(metric)
+    if int(current) - int(healthy) > deviation:
+        return False
+    time.sleep(1)
     return True
 
+def retrieve_metric(query):
+    response = requests.get(PROM_URL+"/api/v1/query?query="+query).content
+    metric = json.loads(response)
+    if metric["data"]["result"]:
+        print(metric["data"]["result"][0]["value"][1])
+        return metric["data"]["result"][0]["value"][1]
+    print(metric)
+    return 0
 
 def update_destination_rule(crds, namespace, service):
     destination_rule = crds.get_namespaced_custom_object("networking.istio.io", 
@@ -28,13 +35,21 @@ def update_destination_rule(crds, namespace, service):
     canary_index = next((index for (index, d) in enumerate(destination_rule["spec"]["subsets"]) 
                                                                 if d["name"] == "canary"), None)
     destination_rule["spec"]["subsets"][canary_index]["labels"]["version"] = obj["spec"]["canary_version"]
+    destination_rule = cleanup_k8s_object(destination_rule)
     crds.patch_namespaced_custom_object("networking.istio.io", 
                                         "v1alpha3", 
                                         namespace, 
                                         "destinationrules", 
                                         name, 
                                         destination_rule)
-     
+    return destination_rule
+         
+def cleanup_k8s_object(obj):
+    obj["metadata"].pop("annotations")
+    obj["metadata"].pop("resourceVersion")    
+    obj["metadata"].pop("uid")   
+    obj["metadata"].pop("selfLink")
+    return obj
 
 def update_virtualservice(crds, obj):
     metadata = obj.get("metadata")
@@ -45,22 +60,37 @@ def update_virtualservice(crds, obj):
     namespace = metadata.get("namespace")
     obj["spec"]["inprocess"] = True
     service = obj["spec"]["service"]
+    metric = obj["spec"]["metric"]
+    
+    if 'deviation' in obj["spec"]:
+        deviation = int(obj["spec"]["deviation"])
+    else:
+        deviation = 10
+    
+    if 'if_unhealthy' in obj["spec"]:
+        if_unhealthy = obj["spec"]["if_unhealthy"]
+    else:
+        if_unhealthy = 'rollback'
+
+    if 'increment' in obj["spec"]:
+        increment = int(obj["spec"]['increment'])
+    else:
+        increment = 1
+
+    healthy = retrieve_metric(metric)
     print("Updating: %s" % name)
     canary_healthy = True
-    prodWeight = 99
-    canaryWeight = 1
+    prodWeight = 100
+    canaryWeight = 0
     
-    update_destination_rule(crds, namespace, service)
+    destination_rule = update_destination_rule(crds, namespace, service)
 
     vs = crds.get_namespaced_custom_object("networking.istio.io", "v1alpha3", namespace, "virtualservices", service)
     canary_index = next((index for (index, d) in enumerate(vs["spec"]["http"][0]["route"]) 
                                                                 if d["destination"]["subset"] == "canary"), None)
     prod_index = next((index for (index, d) in enumerate(vs["spec"]["http"][0]["route"]) 
                                                                 if d["destination"]["subset"] == "production"), None)
-    vs["metadata"].pop("annotations")
-    vs["metadata"].pop("resourceVersion")    
-    vs["metadata"].pop("uid")   
-    vs["metadata"].pop("selfLink") 
+    vs = cleanup_k8s_object(vs)
     
     
     while canary_healthy and canaryWeight <=100:
@@ -68,33 +98,57 @@ def update_virtualservice(crds, obj):
         vs["spec"]["http"][0]["route"][canary_index]["weight"] = canaryWeight
         vs["spec"]["http"][0]["route"][prod_index]["weight"] = prodWeight
         crds.patch_namespaced_custom_object("networking.istio.io", "v1alpha3", namespace, "virtualservices", name, vs)
-        canary_healthy = check_canary_health()
-        prodWeight-=1
-        canaryWeight+=1
-    rollback = """
-apiVersion: networking.istio.io/v1alpha3
-kind: VirtualService
-metadata:
-  name: {name}
-spec:
-  hosts:
-  - {name}
-  http:
-  - route:
-    - destination:
-        host: {name}
-        port:
-          number: 80
-        subset: v02
-      weight: 100
-    - destination:
-        host: aleph
-        port:
-          number: 80
-        subset: v03
-      weight: 0""".format(name=name)
-    crds.patch_namespaced_custom_object("networking.istio.io", "v1alpha3", namespace, "virtualservices", name, yaml.load(rollback))  
+        canary_healthy = check_canary_health(metric, healthy, deviation)
+        prodWeight-=increment
+        canaryWeight+=increment
+    
+    if canaryWeight >= 100:
+        release_canary(destination_rule, vs, crds, obj)
+    else:
+        exec(if_unhealthy+'(crds, vs, namespace, name)')
+  
+def freeze(crds, vs, namespace, name):
+    print("Freezing the canary")
+    return True
 
+def rollback(crds, virtualservice, namespace, name):
+    print("Rolling back")
+    canary_index = next((index for (index, d) in enumerate(virtualservice["spec"]["http"][0]["route"]) 
+                                                                if d["destination"]["subset"] == "canary"), None)
+    prod_index = next((index for (index, d) in enumerate(virtualservice["spec"]["http"][0]["route"]) 
+                                                                if d["destination"]["subset"] == "production"), None)
+    virtualservice["spec"]["http"][0]["route"][canary_index]["weight"] = 0
+    virtualservice["spec"]["http"][0]["route"][prod_index]["weight"] = 100
+    crds.patch_namespaced_custom_object("networking.istio.io", 
+                                        "v1alpha3", 
+                                        namespace, 
+                                        "virtualservices", 
+                                        name, 
+                                        virtualservice)
+
+
+def release_canary(destination_rule, virtualservice, crds, obj):
+    #in destinationrule - make prod point at canary
+    #update destination rule
+    prod_index = next((index for (index, d) in enumerate(destination_rule["spec"]["subsets"]) 
+                                                                if d["name"] == "production"), None)
+    destination_rule["spec"]["subsets"][prod_index]["labels"]["version"] = obj["spec"]["canary_version"]
+    crds.patch_namespaced_custom_object("networking.istio.io", 
+                                        "v1alpha3", 
+                                        obj["metadata"]["namespace"], 
+                                        "destinationrules", 
+                                        obj["metadata"]["name"], 
+                                        destination_rule)
+    #in virtualservice - set weights: prod=100 and canary=0
+    rollback(crds, virtualservice, obj["metadata"]["namespace"], obj["metadata"]["name"])
+    #now delete the CR
+    crds.delete_namespaced_custom_object(DOMAIN, 
+                                         "v1alpha1", 
+                                         obj["metadata"]["namespace"], 
+                                         "birdwatches", 
+                                         obj["metadata"]["name"],
+                                         {})
+         
 if __name__ == "__main__":
     if 'KUBERNETES_PORT' in os.environ:
         config.load_incluster_config()
